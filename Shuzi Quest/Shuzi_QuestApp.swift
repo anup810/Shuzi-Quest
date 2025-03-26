f2a97f93d83eca89b350a6413499b261e53ee092
Anup Saud <anupsaud10@gmail.com>
2025-03-25 20:26:33 -0400

    Inital project structure

---
+++ Shuzi Quest/Shuzi_QuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shuzi_QuestApp.swift
+//  Shuzi Quest
+//
+//  Created by Anup Saud on 2025-03-25.
+//
+
+import SwiftUI
+
+@main
+struct Shuzi_QuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
